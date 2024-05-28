f12ac3c79de5874fd8df6c828b513916667b22f0
Fatih Gumus <fatihgumus72@gmail.com>
2024-05-28 20:59:14 +0300

    Initial Commit

---
+++ SwiftState/SwiftStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStateApp.swift
+//  SwiftState
+//
+//  Created by Fatih Gümüş on 28.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
